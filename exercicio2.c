3d907f9aad5312aeade8c1673308e3eeee89ec13
fabiohbfreitas <difreitas.fabio@gmail.com>
2023-05-02 23:51:20 -0300

    fix exercise 2

--- exercicio2.c
+++ exercicio2.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FUNCIONARIOS_MAX 1024
+#define FUNCIONARIOS_MAX 256
 
 int pecas[FUNCIONARIOS_MAX] = {};
-int funcionarios = 1;
+int funcionarios = 0;
 
-int validaQuantidade(int pecas) { return pecas != -1 && pecas > 0; }
+int validaQuantidade(int pecas) { return !(pecas != -1 && pecas > 0); }
 
 float calculaSalario(int pecas) {
   const float salario_base = 600.f;
@@ -18,21 +18,16 @@ float calculaSalario(int pecas) {
     bonus = 0.f;
   } else if (pecas <= 80) {
     bonus += (pecas - 50) * 0.5;
-    printf("\tbonus atual: %.4f\n", bonus);
   } else {
-    const int bonus_parcial = (pecas - 80) * 0.75;
-    bonus += 30 * 0.5 +
-             bonus_parcial; // 30 -> 80 - 50 já que sempre será maior que 80
-    printf("\tbonus atual: %.4f\n", bonus);
+    const float bonus_parcial = (pecas - 80) * 0.75;
+    bonus += 30 * 0.5 + bonus_parcial; // 30 -> 80 - 50 já que sempre será maior que 80
   }
   return salario_base + bonus;
 }
 
 void mostraFinal() {
-  printf("------ Salários\n\n");
-  //printf("funcs: %d \n", n_funcionarios);
+  printf("\n------ Salários\n");
   for (int i = 0; i < funcionarios; i += 1) {
-    //printf("F%d Idx %d pcs %d\n", i + 1, i, pecas[i]);
     printf("Funcionario %d: R$%.2f\n", i + 1, calculaSalario(pecas[i]));
   }
 }
@@ -41,25 +36,21 @@ int main(void) {
   setlocale(LC_ALL, "Portuguese");
 
   while (1) {
-    printf("funcionarios: %d\n", funcionarios);
-    printf("Peças do funcionario %d (ou -1 para finalizar): ", funcionarios);
     int numero_atual = 0;
-    scanf("%d", &numero_atual);
-    if (numero_atual == -1) {
-      funcionarios -= 1;
-      break;
-    } else {
+    while (validaQuantidade(numero_atual)) {
+      printf("Peças do funcionario %d (ou -1 para finalizar)\n> ", funcionarios + 1);
+      scanf("%d", &numero_atual);
+      if (numero_atual == -1) {
+        goto finaliza;
+      }
       if (validaQuantidade(numero_atual)) {
-        funcionarios += 1;
-        int idx = funcionarios - 1;
-        pecas[idx] = numero_atual;
-      } /*else {
-        printf("[Erro] Número de peças inválido\n\n");
-        continue;
-      }*/
-      
+        printf("\t[Erro] Numero de peças invalido.\n");
+      }
     }
+    pecas[funcionarios] = numero_atual;
+    funcionarios += 1;
   }
-  mostraFinal();
+  finaliza:
+    mostraFinal();
   return EXIT_SUCCESS;
 }
